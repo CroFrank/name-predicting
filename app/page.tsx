8689d4d26d97c5e85606f11ee61462500476384e
Fran <fducekuce@gmail.com>
2023-09-22 11:06:11 +0200

    organise code

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,11 @@
-'use client'
-
-import { useRouter } from "next/navigation"
-import { FormEvent, useState } from "react"
+import Form from "./components/Form";
 
 export default function Home() {
-  const [input, setInput] = useState('')
-  const router = useRouter()
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault()
-    router.push(`/prediction/${input}`)
-  }
-
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-lg mx-auto mt-8 p-6 bg-white rounded-lg shadow-md w-2/4">
         <h1 className="text-3xl font-semibold text-center mb-6">Name Prediction</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-6">
-            <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Choose a name for prediction</label>
-            <input
-              id="name"
-              type="text"
-              placeholder='Enter name'
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-            >
-              Show predictions
-            </button>
-          </div>
-        </form>
+        <Form />
       </div>
     </div>
   )
--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -1,3 +1,6 @@
+import { countrysList } from "@/app/_countries-list/c-list";
+import Link from "next/link";
+
 interface Params {
     params: { name: string }
 }
@@ -33,28 +36,33 @@ export default async function Prediction({ params }: Params) {
     ]);
 
     return (
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
-            <div className="p-8">
-                <div className="uppercase tracking-wide text-lg text-indigo-500 font-semibold">
-                    Here are stats and most common cases for name <b>{params.name}:</b>
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
+                <div className="p-8">
+                    <div className="uppercase tracking-wide text-lg text-indigo-500 font-semibold">
+                        Here are stats and most common cases for name <b>{params.name}:</b>
+                    </div>
+                    <div className="block mt-1 text-lg leading-tight font-medium text-black">
+                        Age: <b>{age?.age}</b>
+                    </div>
+                    <div className="block mt-1 text-lg leading-tight font-medium text-black">
+                        Gender: <b>{gender?.gender}</b> with a probability off: {gender?.probability * 100}%
+                    </div>
+                    <div className="block mt-1 text-lg leading-tight font-medium text-black">
+                        Nationality: {nationality?.country.map((c: Country) => {
+                            return <div key={crypto.randomUUID()}>
+                                <p className="inline">
+                                    <b>{countrysList.filter((v) => (v.code === c.country_id)).length === 0 ? c.country_id : countrysList.filter((v) => (v.code === c.country_id))[0].name}</b>
+                                </p> -
+                                <p className="inline"> with a probability of {(c.probability * 100).toFixed(2)}%</p>
+                            </div>
+                        })}
+                    </div>
+                    <div className="block mt-1 text-lg leading-tight font-medium text-black">
+                        <b>{gender?.count}</b> people with name {params.name} in the world
+                    </div>
                 </div>
-                <div className="block mt-1 text-lg leading-tight font-medium text-black">
-                    Age: <b>{age?.age}</b>
-                </div>
-                <div className="block mt-1 text-lg leading-tight font-medium text-black">
-                    Gender: <b>{gender?.gender}</b> with a probability off: {gender?.probability * 100}%
-                </div>
-                <div className="block mt-1 text-lg leading-tight font-medium text-black">
-                    Nationality: {nationality?.country.map((c: Country) => {
-                        return <div key={crypto.randomUUID()}>
-                            <p className="inline"><b>{c.country_id}</b></p> -
-                            <p className="inline"> with a probability of {(c.probability * 100).toFixed(2)}%</p>
-                        </div>
-                    })}
-                </div>
-                <div className="block mt-1 text-lg leading-tight font-medium text-black">
-                    <b>{gender?.count}</b> people with name {params.name} in the world
-                </div>
+                <Link href={'/'} className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600">New name request?</Link>
             </div>
         </div>
     );
